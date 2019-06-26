59b26e6d677e94b778ae372c19cd65bf84af6e1a
Nik Komarov <nik-luga@mail.ru>
2019-06-26 17:19:37 +0300

    Add project files.

---
+++ inspinia-playground/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace inspinia_playground.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
