18b9284ec5e8c7b5a4eb10e2fdf5cdcc7d3855fc
Nik Komarov <nik-luga@mail.ru>
2019-06-26 23:39:04 +0300

    Добавлен datatables server-side

--- inspinia-playground/Controllers/OrderController.cs
+++ inspinia-playground/Controllers/OrderController.cs
@@ -9,6 +9,8 @@ using Data;
 using System.ComponentModel.DataAnnotations;
 using inspinia.Models.Requests;
 using inspinia.Models;
+using inspinia.Models.Datatables;
+using System.Linq.Expressions;
 
 namespace inspinia_playground.Controllers
 {
@@ -54,6 +56,47 @@ namespace inspinia_playground.Controllers
             };
 
             return Json(response);
+        }
+
+        [HttpPost]
+        public IActionResult GetDatatable(DataTableAjaxPostModel request)
+        {
+            Expression<Func<Data.Order, bool>> predicate = order => true;
+
+            if (!string.IsNullOrWhiteSpace(request.search?.value))
+            {
+                var search = request.search.value.ToLower();
+
+                predicate = order => order.Client.ToLower().Contains(search)
+                    || order.Email.ToLower().Contains(search)
+                    || order.Status.ToLower().Contains(search)
+                    //TODO: придумать что-то умнее
+                    || order.Sum.ToString() == search;
+            }
+            var entities = _data.Orders
+                .Where(predicate)
+                .Skip(request.start)
+                .Take(request.length);
+
+            var total = _data.Orders.Count();
+            var totalFilter = _data.Orders.Where(predicate).Count();
+
+            var orders = entities.Select(m => new OrderItemResponse
+                {
+                    Id = m.Id,
+                    Client = m.Client,
+                    Email = m.Email,
+                    Status = m.Status,
+                    Sum = m.Sum
+                });
+
+            return Json(new
+            {
+                draw = request.draw,
+                recordsTotal = total,
+                recordsFiltered = totalFilter,
+                data = orders
+            });
         }
     }
 }
